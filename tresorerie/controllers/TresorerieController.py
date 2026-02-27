765295413cf156d3ac50accbe6ca3256c5e3d5d9
tafita37 <118960434+tafita37@users.noreply.github.com>
2026-02-28 00:43:24 +0300

    fix(authentification_required)

--- tresorerie/controllers/TresorerieController.py
+++ tresorerie/controllers/TresorerieController.py
@@ -1,7 +1,9 @@
 # views.py
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 
 @require_GET
+@login_required(login_url='login_user_page') 
 def tresorerie_page(request):
     return render(request, "views/tresorerie.html")
